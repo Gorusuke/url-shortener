2b5605b22e745e07da8ad7fd18deae71ea4cb97d
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-03-12 17:49:40 -0500

    Add icon components and update interfaces

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,11 +1,27 @@
+import { MouseEvent } from "react"
+
 export interface LinksData {
   rowid: number
   shortUrl: string
   originalUrl: string
 }
 
-export interface copyIconProps {
+export interface CopyIconProps {
   className: string
   link: string
   setCopy: (arg: boolean) => void
+}
+
+
+export interface InputSearchProps {
+  urlText: string
+  setUrlText: (arg: string) => void
+  isEditing: boolean
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void
+}
+
+export interface TableLinkProps {
+  deleteShortUrl: (arg: string) => void
+  onClick: (arg: string) => void
+  lastLink: string
 }
