0d05b372ee650e7e0782a46ba7eebb5f2bb0d2fa
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-03-14 12:08:33 -0500

    Add auth helper and change nextauth route to get authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,2 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-
-
-const handler = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || ""
-    })
-  ]
-});
-
-export { handler as GET, handler as POST}
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
