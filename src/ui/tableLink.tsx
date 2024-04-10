c7cb87d4dd12e7ee67af4b5683952c39f6f97976
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-04-10 11:51:10 -0500

    Add loading to table links and update css styles

--- src/ui/tableLink.tsx
+++ src/ui/tableLink.tsx
@@ -1,9 +1,10 @@
+import { useEffect, useState } from 'react'
+import { LinksData, TableLinkProps } from '@/lib/interfaces'
 import Tooltip from './tooltip'
 import CopyIcon from './Icons/copyIcon'
 import DeleteIcon from './Icons/deleteIcon'
 import EditIcon from './Icons/editIcon'
-import { useEffect, useState } from 'react'
-import { LinksData, TableLinkProps } from '@/lib/interfaces'
+import TableSkeleton from './tableSkeleton'
 import styles from '@/app/page.module.css'
 
 
@@ -11,52 +12,60 @@ const TableLink = ({deleteShortUrl, onClick, lastLink}: TableLinkProps) => {
   const [copy, setCopy] = useState(false)
   const [allLinks, setAllLinks] = useState<LinksData[]>([])
   const [route, setRoute] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     setRoute(window.location.origin)
     const getLinks = async () => {
       const allLinks = await fetch('/api/links')
       const links = await allLinks.json()
+      setLoading(false)
       setAllLinks(links.data);
     }
     getLinks()
   }, [lastLink])
   
-  return Boolean(allLinks.length) &&
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th className={`${styles.cell} ${styles.cells}`}>Short Link</th>
-          <th className={`${styles.cell} ${styles.cells}`}>Original Link</th>
-          <th className={`${styles.cell} ${styles.cells}`}>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {allLinks.toReversed().map((link, idx) => 
-          <tr key={link.shortUrl + idx}>
-            <td className={styles.cells}>
-              <div className={styles.cellWithIcon}>
-                {`${route}/go/${link.shortUrl}`}
-                <Tooltip wasCopied={copy}>
-                  <CopyIcon 
-                    className={styles.copyIcon} 
-                    setCopy={setCopy}
-                    link={`${route}/go/${link.shortUrl}`} 
-                  />
-                </Tooltip>
-              </div>
-            </td>
-            <td className={`${styles.cells} ${styles.longCell}`}>{link.originalUrl}</td>
-            <td className={styles.cells}>
-              <div className={styles.cellWithIcon}>
-                <EditIcon onClick={() => onClick(link.originalUrl, link.shortUrl)} />
-                <DeleteIcon onClick={() => deleteShortUrl(link.shortUrl)} />
-              </div>
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+  return (
+    <>
+      {loading && <TableSkeleton />}
+      {!loading && Boolean(allLinks.length) &&
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={`${styles.cell} ${styles.cells}`}>Short Link</th>
+              <th className={`${styles.cell} ${styles.cells}`}>Original Link</th>
+              <th className={`${styles.cell} ${styles.cells}`}>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allLinks.toReversed().map((link, idx) => 
+              <tr key={link.shortUrl + idx}>
+                <td className={styles.cells}>
+                  <div className={styles.cellWithIcon}>
+                    {`${route}/go/${link.shortUrl}`}
+                    <Tooltip wasCopied={copy}>
+                      <CopyIcon 
+                        className={styles.copyIcon} 
+                        setCopy={setCopy}
+                        link={`${route}/go/${link.shortUrl}`} 
+                      />
+                    </Tooltip>
+                  </div>
+                </td>
+                <td className={`${styles.cells} ${styles.longCell}`}>{link.originalUrl}</td>
+                <td className={styles.cells}>
+                  <div className={styles.cellWithIcon}>
+                    <EditIcon onClick={() => onClick(link.originalUrl, link.shortUrl)} />
+                    <DeleteIcon onClick={() => deleteShortUrl(link.shortUrl)} />
+                  </div>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      }
+    </>
+  )
 }
 
 export default TableLink
