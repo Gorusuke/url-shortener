a2e6796812090c3e03ea8a5ac9ee400c86faa0a3
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-04-10 09:50:00 -0500

    Update title and descriptions in page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,14 @@
 import { auth } from '@/auth';
 import { redirect } from "next/navigation";
 import DashboardUI from "@/ui/dashboardUI";
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: "Dashbord",
+  description: "Dashbord url",
+};
 
 export default async function Dashboard () {
   const session = await auth();
@@ -8,6 +16,10 @@ export default async function Dashboard () {
   if(!session?.user) {
     redirect('/')
   }
-
-  return <DashboardUI user={session?.user}/>
+  
+  return (
+    <Suspense fallback={<div>Loading.......</div>}>
+      <DashboardUI user={session?.user}/>
+    </Suspense>
+  )
 }
